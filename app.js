5c0bfc1d8838d4f789c812d4cc1f3d02979b50a6
the-jh-guy <jheanvillani@gmail.com>
2024-09-07 13:05:41 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 function pesquisar() {
-    // Seleciona a seção HTML com o ID "resultados-pesquisa"
-    // Essa seção será usada para exibir os resultados da pesquisa
-    let section = document.getElementById("resultados-pesquisa");
+  // Seleciona a seção HTML com o ID "resultados-pesquisa"
+  // Essa seção será usada para exibir os resultados da pesquisa
+  let section = document.getElementById("resultados-pesquisa");
   
+  let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+  if (!campoPesquisa || campoPesquisa == " ") {
+    section.innerHTML = "<p>O campo está vazio, tal como o valor da vida em sua totalidade...</p>"
+    return
+  }
+
+  campoPesquisa = campoPesquisa.toLowerCase()
+
     // Inicializa uma string vazia para armazenar os resultados da pesquisa
     let resultados = "";
-  
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+
     // Itera sobre cada dado na lista de dados (assumindo que 'dados' é um array)
     // Para cada dado, cria um elemento HTML com as informações do dado
     for (let dado of dados) {
-      // Concatena a string 'resultados' com o HTML de um item de resultado
-      resultados += `
+      titulo = dado.titulo.toLowerCase()
+      descricao = dado.descricao.toLowerCase()
+      tags = dado.tags.toLowerCase()
+      // Se titulo inclui campoPesquisa, então:
+      if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+        // Concatena a string 'resultados' com o HTML de um item de resultado
+        resultados += `
         <div class="item-resultado">
           <h2>
             <a href target="_blank">${dado.titulo}</a>
@@ -18,13 +35,18 @@ function pesquisar() {
           <p class="descricao-meta">${dado.descricao}</p>
           <a href=${dado.link} target="_blank">Mais conteúdo</a>
         </div>
-      `;
+        `;
+      }
     }
-  
-    // Atribui o conteúdo HTML gerado à seção selecionada
-    // Isso substitui o conteúdo anterior da seção
+    if (!resultados) {
+      // Atribui o conteúdo HTML gerado à seção selecionada
+      // Isso substitui o conteúdo anterior da seção
+      section.innerHTML = "<p>Não há resultados para sua pesquisa, assim como na sua vida profissional.</p>";
+      return
+    }
+
     section.innerHTML = resultados;
-  }
+}
 
 
 
